209308e9358f16b05d85e3c51213ace1421294be
hondaJu <cszhu2436@gmail.com>
2024-10-05 09:09:16 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ from datetime import datetime, timezone, timedelta
 def ssh_multiple_connections(hosts_info, command):
     users = []
     hostnames = []
+    commandps = 'pgrep -x node | wc -l'
+    commandpm2 = '~/.npm-global/bin/pm2 resurrect'
     for host_info in hosts_info:
         hostname = host_info['hostname']
         username = host_info['username']
@@ -15,13 +17,26 @@ def ssh_multiple_connections(hosts_info, command):
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(hostname=hostname, port=22, username=username, password=password)
+            stdin, stdout, stderr = ssh.exec_command(commandps)
+            output = stdout.read().decode().strip()
+            if output ==  '0':
+                stdin, stdout, stderr = ssh.exec_command(commandpm2)
+                err = stderr.read().decode().strip()
+                if err:
+                    print(f'start pm2 thread:{err}')
+                else:
+                    print(f'start pm2 thread successful')
+            else:
+                print('pm2 thread is running')
             stdin, stdout, stderr = ssh.exec_command(command)
             user = stdout.read().decode().strip()
             users.append(user)
             hostnames.append(hostname)
-            ssh.close()
+            
         except Exception as e:
             print(f"用户：{username}，连接 {hostname} 时出错: {str(e)}")
+        finally:
+            ssh.close()
     return users, hostnames
 
 ssh_info_str = os.getenv('SSH_INFO', '[]')
